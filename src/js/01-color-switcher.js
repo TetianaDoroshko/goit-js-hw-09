eb30f9c525e93b1dc62f0a20afcf7255c6481827
Tetiana Doroshko <yanisolua@gmail.com>
2022-06-03 14:51:02 +0300

    Adds resolve task-01.

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let intervalID;
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+refs.stopBtn.disabled = true;
+
+function onStartBtnClick(event) {
+  if (intervalID) {
+    return;
+  }
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  document.body.style.backgroundColor = getRandomHexColor();
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalID);
+  intervalID = null;
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
